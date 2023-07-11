97cc326602208e6602fde068fa1869bdce286788
kuspilla <118829965+kuspilla@users.noreply.github.com>
2023-07-11 15:33:39 +0530

    Code changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A{
+        public String math(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public  String math(){
+            return "Method is overridden in Extendend class B" ;
+        }
+    }
+
+    public static void Main(String[] args){
+        B obj = new B();
+        obj.math();
+        B obj1 = new B();
+        obj1.math();
+    }
 }
